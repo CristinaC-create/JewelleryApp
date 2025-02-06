203cd350848a19d64cce501e0688f084882fa97c
Cristina <Cristina.Cabral@triosstudent.com>
2025-02-05 19:52:07 -0500

    Initial Commit

---
+++ JewelleryApp/JewelleryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JewelleryAppApp.swift
+//  JewelleryApp
+//
+//  Created by Cristina Cabral on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct JewelleryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
